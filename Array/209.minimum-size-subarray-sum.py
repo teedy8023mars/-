f764c84a7f81bf770c0afcdb020a97cb6ab296a1
ImtheTed <31605638+teedy8023mars@users.noreply.github.com>
2022-06-29 19:09:11 +0800

    双指针sub-array

--- Array/209.minimum-size-subarray-sum.py
+++ Array/209.minimum-size-subarray-sum.py
@@ -6,19 +6,15 @@
 
 # @lc code=start
 class Solution:
-    def minSubArrayLen(self, s: int, nums: List[int]) -> int:
-        # 定义一个无限大的数
-        res = float("inf")
-        Sum = 0
-        index = 0
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        res = float('inf')
+        sum=index=0
         for i in range(len(nums)):
-            Sum += nums[i]
-            while Sum >= s:
+            sum += nums[i]
+            while sum>=target:
                 res = min(res, i-index+1)
-                Sum -= nums[index]
+                sum -= nums[index]
                 index += 1
-        return 0 if res==float("inf") else res
-
-        
+        return 0 if res == float('inf') else res
 # @lc code=end
 
