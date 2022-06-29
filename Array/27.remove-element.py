3cff1619f7d8798ff24c84a9533a7ffb4e564626
ImtheTed <31605638+teedy8023mars@users.noreply.github.com>
2022-06-29 15:06:41 +0800

    27

--- Array/27.remove-element.py
+++ Array/27.remove-element.py
@@ -7,6 +7,12 @@
 # @lc code=start
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        
+        fast=slow=0
+        while fast<len(nums):
+            if nums[fast]!=val:
+                nums[slow]=nums[fast]
+                slow+=1
+            fast+=1
+        return slow
 # @lc code=end
 
