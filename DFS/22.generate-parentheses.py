0534721ea3ed9902cb23369a5ac4449b7dc3427e
ImtheTed <31605638+teedy8023mars@users.noreply.github.com>
2022-07-01 15:05:20 +0800

    generate parentheses

---
+++ DFS/22.generate-parentheses.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode id=22 lang=python3
+#
+# [22] Generate Parentheses
+#
+
+# @lc code=start
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = []
+        def dfs(n,l,r,s):
+            if (l==n and r==n):
+                res.append(s)
+            if l<n:
+                dfs(n,l+1,r,s+'(')
+            if (r<n and r<l):
+                dfs(n,l,r+1,s+')')
+        dfs(n,0,0,'')
+        return res
+# @lc code=end
+
