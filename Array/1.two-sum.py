5880167915de072235419b2cd90edf1530d85239
ImtheTed <31605638+teedy8023mars@users.noreply.github.com>
2022-07-02 01:47:39 +0800

    2 solutions

--- Array/1.two-sum.py
+++ Array/1.two-sum.py
@@ -7,10 +7,18 @@
 # @lc code=start
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # seen = {}
+        # for i,v in enumerate(nums):
+        #     r = target - v
+        #     if r in seen:
+        #         return [i,seen[r]] 
+        #     else:
+        #         seen[v] = i
         for i in range(len(nums)):
             for j in range(i+1,len(nums)):
-                if target - nums[i] == nums[j]:
+                if target - nums[j] == nums[i]:
                     return [i,j]
-
+                else:
+                    continue
 # @lc code=end
 
