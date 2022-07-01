f608cf135a0633b97f104b7e97d5e872936276f0
ImtheTed <31605638+teedy8023mars@users.noreply.github.com>
2022-07-02 01:48:07 +0800

    螺旋队列

---
+++ LinkedList/142.linked-list-cycle-ii.py
@@ -0,0 +1,30 @@
+#
+# @lc app=leetcode id=142 lang=python3
+#
+# [142] Linked List Cycle II
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+        slow = fast = head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            if fast==slow:
+                p=head
+                q=slow 
+                while p!=q:
+                    p = p.next
+                    q = q.next 
+                return p
+        return None
+# @lc code=end
+
