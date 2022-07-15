12de76b353088ef20f9e854ae658373f810bff13
ImtheTed <31605638+teedy8023mars@users.noreply.github.com>
2022-07-16 00:56:53 +0800

    divmod

--- LinkedList/2.add-two-numbers.py
+++ LinkedList/2.add-two-numbers.py
@@ -11,21 +11,21 @@
 #         self.val = val
 #         self.next = next
 
+from typing import List
+
+
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         ret = r = ListNode(0)
-        sum = carry = 0
-        while l1 or l2:
+        carry = 0
+        while l1 or l2 or carry:
             x = l1.val if l1 else 0
             y = l2.val if l2 else 0
-            sum = x + y + carry 
-            carry = sum//10
-            r.next = ListNode(sum%10)
-            r = r.next
-            if l2: l2 = l2.next
+            carry, val = divmod(x+y+carry, 10)
+            r.next = ListNode(val)
             if l1: l1 = l1.next
-        if carry > 0:
-            r.next = ListNode(1)
+            if l2: l2 = l2.next
+            r = r.next
         return ret.next
 
 # @lc code=end
