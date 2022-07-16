cff6383b23ae390a0bb48595e32ef500334954e5
ImtheTed <31605638+teedy8023mars@users.noreply.github.com>
2022-07-16 15:49:07 +0800

    -

---
+++ Hash/349.intersection-of-two-arrays.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode id=349 lang=python3
+#
+# [349] Intersection of Two Arrays
+#
+
+# @lc code=start
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        ret=[]
+        for i in nums1:
+            if i not in ret and i in nums2:
+                ret.append(i)
+        return ret
+
+# @lc code=end
+
