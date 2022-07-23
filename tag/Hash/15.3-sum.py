028d4f82e462ccaa04aa291b9986a5f5480c5521
ImtheTed <31605638+teedy8023mars@users.noreply.github.com>
2022-07-24 01:27:17 +0800

    -

--- tag/Hash/15.3-sum.py
+++ tag/Hash/15.3-sum.py
@@ -10,29 +10,27 @@ from collections import defaultdict
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         ans = []
+        n = len(nums)
         nums.sort()
-        for i in range(len(nums)):
+        for i in range(n):
             left = i + 1
-            right = len(nums) - 1
+            right = n - 1
             if nums[i] > 0:
-            # impossible to sum up to zero
-                continue
-            # 去重：nums[i]要加进去sum，下一次遍历的时候不把相同的计算进去
-            if i>=1 and nums[i] == nums[i-1]:
+                break
+            if i >= 1 and nums[i] == nums[i - 1]:
                 continue
             while left < right:
-                sum = nums[i] + nums[left] + nums[right]
-                if sum>0:
+                total = nums[i] + nums[left] + nums[right]
+                if total > 0:
                     right -= 1
-                elif sum<0:
+                elif total < 0:
                     left += 1
                 else:
-                    #[-2,-2,0,0,2,2]
-                    ans.append([nums[i],nums[left],nums[right]])
+                    ans.append([nums[i], nums[left], nums[right]])
+                    while left != right and nums[left] == nums[left + 1]: left += 1
+                    while left != right and nums[right] == nums[right - 1]: right -= 1
                     left += 1
-                    # 与i的去重思路相同，下一次遍历相同的left和right不计入sum
-                    while left < right and nums[left]==nums[left+1]: 
-                        left += 1
+                    right -= 1
         return ans
 
 # @lc code=end
