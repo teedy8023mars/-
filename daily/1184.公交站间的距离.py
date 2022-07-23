3bcd74aec9240ee6edb4b44b7ce2289716c7d52b
ImtheTed <31605638+teedy8023mars@users.noreply.github.com>
2022-07-24 03:25:38 +0800

    daily

---
+++ daily/1184.公交站间的距离.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode.cn id=1184 lang=python3
+#
+# [1184] 公交站间的距离
+#
+
+# @lc code=start
+from calendar import c
+
+
+class Solution:
+    def distanceBetweenBusStops(self, distance: List[int], start: int, destination: int) -> int:
+        if start>destination:
+            start,destination = destination,start
+        v = sum(distance[start:destination])
+        return min(v, sum(distance)-v)
+        
+        
+        
+            
+            
+# @lc code=end
+
