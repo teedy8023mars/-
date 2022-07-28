db20fc6c365b485b62c4eedc80c43ce7b9d79001
ImtheTed <31605638+teedy8023mars@users.noreply.github.com>
2022-07-29 04:32:03 +0800

    updated solution

--- daily/347.前-k-个高频元素.py
+++ daily/347.前-k-个高频元素.py
@@ -10,14 +10,29 @@ import re
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        seen = {}
-        for i,v in enumerate(nums):
-            seen[v] = 1 if v not in seen else seen[v]+1
+        """solution 1"""
+        # seen = {}
+        # for i,v in enumerate(nums):
+        #     seen[v] = 1 if v not in seen else seen[v]+1
+        # ret = []
+        # seen = dict(sorted(seen.items(),key=lambda x:x[1],reverse=True))            
+        # for i in list(seen.keys()):
+        #     ret.append(i)
+        # return ret[:k]
+        
+        """solution 2"""
+        # init a counter
+        hash = {}
+        for num in nums:
+            hash[num] = 1 if num not in hash else hash[num]+1 
+        keys = list(hash.keys())
+        vals = list(hash.values())
         ret = []
-        seen = dict(sorted(seen.items(),key=lambda x:x[1],reverse=True))            
-        for i in list(seen.keys()):
-            ret.append(i)
-        return ret[:k]
-            
+        for i in range(k):
+            m = max(vals)
+            index = vals.index(m)
+            vals[index] = 0
+            ret.append(keys[index])
+        return ret
 # @lc code=end
 
