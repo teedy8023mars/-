bbc49d62d3c435a387e7281e567d784e043811b0
ImtheTed <31605638+teedy8023mars@users.noreply.github.com>
2022-07-29 23:31:10 +0800

    modified

--- tag/Hash/454.4-sum-ii.py
+++ tag/Hash/454.4-sum-ii.py
@@ -7,21 +7,17 @@
 # @lc code=start
 class Solution:
     def fourSumCount(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]) -> int:
-        hashmap = dict()
+        hash = dict()
         for n1 in nums1:
             for n2 in nums2:
-                if n1 + n2  in hashmap:
-                    hashmap[n1+n2] += 1
-                else:
-                    hashmap[n1+n2] = 1
-        
+                hash[n1+n2] = 1 if n1 + n2  not in hash else hash[n1+n2] + 1
         # if the -(a+b) exists in nums3 and nums4, we shall add the count
         count = 0
         for n4 in nums4:
             for n3 in nums3:
                 key = - n4 - n3
-                if key in hashmap:
-                    count += hashmap[key]
+                if key in hash:
+                    count += hash[key]
         return count
 # @lc code=end
 
