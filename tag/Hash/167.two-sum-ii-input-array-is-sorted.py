d8b2ad6b199b295116801db15de11565c8553759
ImtheTed <31605638+teedy8023mars@users.noreply.github.com>
2022-07-30 17:28:39 +0800

    2 solutions

--- tag/Hash/167.two-sum-ii-input-array-is-sorted.py
+++ tag/Hash/167.two-sum-ii-input-array-is-sorted.py
@@ -7,13 +7,22 @@
 # @lc code=start
 class Solution:
     def twoSum(self, n: List[int], target: int) -> List[int]:
-        left, right = 0, len(n)-1
-        while left < right:
-            if n[left] + n[right] == target:
-                return [left+1, right+1]
-            if n[left] + n[right] > target:
-                right -= 1
-            if n[left] + n[right] < target:
-                left += 1
+        """solution 1: two pointers"""
+        # left, right = 0, len(n)-1
+        # while left < right:
+        #     if n[left] + n[right] == target:
+        #         return [left+1, right+1]
+        #     if n[left] + n[right] > target:
+        #         right -= 1
+        #     if n[left] + n[right] < target:
+        #         left += 1
+
+        """solution 2: hashmap"""
+        hash = {}
+        for i,v in enumerate(n):
+            if target - v in hash:
+                return [hash[target-v]+1, i + 1]
+            else:
+                hash[v] = i
 # @lc code=end
 
