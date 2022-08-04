8551477bd5c98cecdca3abacf0eb127de3ef0b30
ImtheTed <31605638+teedy8023mars@users.noreply.github.com>
2022-08-05 00:59:09 +0800

    happy num

--- tag/3.Hash/202.happy-number.py
+++ tag/3.Hash/202.happy-number.py
@@ -11,17 +11,18 @@ from numpy import record
 class Solution:
     def isHappy(self, n: int) -> bool:
         def find(n):
-            sum = 0
+            total = 0
             while n:
-                sum += (n%10)**2
-                n = n//10
-            return sum 
-        # A set is unordered, unchangeable*, and unindexed
-        seen = set()
-        while 1:
+                total += (n%10)**2
+                n //= 10
+            return total 
+        record = []
+        while True:
             n = find(n)
-            if n==1: return True
-            if n in seen: return False
-            else: seen.add(n) 
+            if n == 1: return True 
+            # n!=1, possible dead loop? check it. 
+            if n in record: return False 
+            # add to hash
+            else: record.append(n)
 # @lc code=end
 
