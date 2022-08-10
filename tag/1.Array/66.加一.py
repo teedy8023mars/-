4ff6ee7b9b94f1d18b49da4afd76872d24b22595
ImtheTed <31605638+teedy8023mars@users.noreply.github.com>
2022-08-10 19:46:54 +0800

    add one

--- tag/1.Array/66.加一.py
+++ tag/1.Array/66.加一.py
@@ -7,14 +7,26 @@
 # @lc code=start
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        a = ''
-        for i in digits:
-            a += str(i)
-        a = str(int(a) + 1)
-        b = []
-        for i in a:
-            b.append(int(i))
-        return b
+        """solution 1:"""
+        # a = ''
+        # for i in digits:
+        #     a += str(i)
+        # a = str(int(a) + 1)
+        # b = []
+        # for i in a:
+        #     b.append(int(i))
+        # return b
+        """solution 2:"""
+        for i in range(len(digits)-1, -1, -1):
+            digits[i] += 1
+            digits[i] %= 10
+            # no carrier, just return
+            if digits[i] != 0:
+                return digits
+        # case when digits[i]==0, means got a carrier
+        digits = [0]*(len(digits)+1)
+        digits[0] = 1
+        return digits
         
             
 # @lc code=end
