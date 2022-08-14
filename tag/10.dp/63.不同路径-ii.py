1f4b4cddb9e74cce818b66e1899dd0fd9cee7884
ImtheTed <31605638+teedy8023mars@users.noreply.github.com>
2022-08-14 22:28:44 +0800

    不同路径

--- tag/10.dp/63.不同路径-ii.py
+++ tag/10.dp/63.不同路径-ii.py
@@ -6,20 +6,29 @@
 
 # @lc code=start
 class Solution:
-    def uniquePathsWithObstacles(self, nums: List[List[int]]) -> int:
-        m,n = len(nums),len(nums[0])
-        cur = [0]*n
-        for j in range(n):
-            if nums[0][j] == 1:
-                break
-            cur[j] = 1
-        
-        for i in range(1,m):
-            for j in range(n):
-                if nums[i][j] == 1:
-                    cur[j] = 0
-                elif j>0:
-                    cur[j] = cur[j]+cur[j-1]
-        return cur[n-1]
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        # 构造一个DP table
+        row = len(obstacleGrid)
+        col = len(obstacleGrid[0])
+        dp = [[0 for _ in range(col)] for _ in range(row)]
+
+        if obstacleGrid[0][0] != 1: dp[0][0] = 1
+        if dp[0][0] == 0: return 0  # 如果第一个格子就是障碍，return 0
+        # 第一行
+        for i in range(1, col):
+            if obstacleGrid[0][i] != 1:
+                dp[0][i] = dp[0][i-1]
+
+        # 第一列
+        for i in range(1, row):
+            if obstacleGrid[i][0] != 1:
+                dp[i][0] = dp[i-1][0]
+        print(dp)
+
+        for i in range(1, row):
+            for j in range(1, col):
+                if obstacleGrid[i][j] != 1:
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        return dp[-1][-1]
 # @lc code=end
 
