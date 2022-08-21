92f25834e7d7e94ea9d1106e6bfa90c08e18ec8a
ImtheTed <31605638+teedy8023mars@users.noreply.github.com>
2022-08-22 00:13:48 +0800

    delete in place

--- 26.删除有序数组中的重复项.py
+++ 26.删除有序数组中的重复项.py
@@ -10,8 +10,10 @@ class Solution:
         x = 1
         for i in range(1,len(nums)):
             if nums[i] != nums[i-1]:
+                # diff, then self-replace
                 nums[x] = nums[i]
                 x += 1
+            # if equal, x stays, i move forward
         return x
 # @lc code=end
 
